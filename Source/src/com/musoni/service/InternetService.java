e55ef65aff3d095417d273f9a79b34b0ed882e30
Shijian Li <95588lsj@gmail.com>
2013-11-16 23:22:23 +0000

    Change handler to asynctask

--- Source/src/com/musoni/service/InternetService.java
+++ Source/src/com/musoni/service/InternetService.java
@@ -13,7 +13,7 @@ import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
 
 import android.annotation.SuppressLint;
-import android.os.Handler;
+import android.os.AsyncTask;
 import android.util.Base64;
 
 public class InternetService implements IService {
@@ -23,7 +23,7 @@ public class InternetService implements IService {
 		
 	}
 
-	public static class HandlerWrapper implements Runnable{
+	public static class HandlerWrapper extends AsyncTask<Void,Void,Void> implements Runnable{
 		
 		private HttpUriRequest req = null;
 		private ResultHandler result = null;
@@ -61,6 +61,13 @@ public class InternetService implements IService {
 				result.fail();
 			}
 			
+		}
+
+		@Override
+		protected Void doInBackground(Void... arg0) {
+						
+			run();
+			return null;
 		}
 	}
 	
@@ -165,9 +172,10 @@ public class InternetService implements IService {
 			req.setParams(parameters);		
 		}
 		
-		HandlerWrapper hw = new HandlerWrapper(req, result);
-		Handler hand = new Handler();
-		hand.post(hw);
+		HandlerWrapper hw = new HandlerWrapper(req, result);	
+		hw.execute();
+		//Handler hand = new Handler();
+		//hand.post(hw);
 	}
 	
 	
