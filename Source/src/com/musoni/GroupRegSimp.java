93154a20ce43830694c29b51f8663c04331de786
wucy <chunyang506@gmail.com>
2013-11-17 05:01:31 +0000

    add group submit

--- Source/src/com/musoni/GroupRegSimp.java
+++ Source/src/com/musoni/GroupRegSimp.java
@@ -5,12 +5,24 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.musoni.service.IService;
+import com.musoni.service.ResultHandler;
+import com.musoni.service.ServiceFactory;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.SimpleAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class GroupRegSimp extends Activity {
 	private Spinner branch_name_spinner;
@@ -54,6 +66,66 @@ public class GroupRegSimp extends Activity {
 	  public void addItemsOnbranch_name_spinner() {
 		  branch_name_spinner = (Spinner) findViewById(R.id.branch_name_spinner);
 		  branch_name_spinner.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+	  }
+	  
+	  int getOfficeID(String x) {
+		  return 1;
+	  }
+	  
+	  int getStaffID(String x) {
+		  return 1;
+	  }
+	  
+	  public void submitClientRegistry(View view) {
+		  
+		  String name = ((EditText) findViewById(R.id.gname)).toString();
+		  String officeid = ((Spinner) findViewById(R.id.branch_name_spinner)).getSelectedItem().toString();
+		  String staffid = ((Spinner) findViewById(R.id.loan_officer_spinner)).getSelectedItem().toString();;
+		  String registration = ((EditText) findViewById(R.id.editText4)).toString();;
+		  JSONArray jarr = new JSONArray();
+		  ListView lv = ((ListView) findViewById(R.id.listView));
+		  for (int i = 0; i < lv.getCount(); ++ i) {
+			  jarr.put(lv.getItemAtPosition(i).toString());
+		  }
+		  
+		  JSONObject json = new JSONObject();
+		  
+		  try {
+			  json.put("name", name);
+			  json.put("officeid", getOfficeID(officeid));
+			  json.put("staffid", getStaffID(staffid));
+			  json.put("registration", registration);
+			  json.put("clientMembers", jarr);
+		  } catch(Exception e) {
+			  e.printStackTrace();
+			  Toast.makeText(getApplicationContext(), "Submit Fail", Toast.LENGTH_LONG).show();
+		  }
+		  
+		  IService service = ServiceFactory.getService();
+		  Toast.makeText(this, "Click", Toast.LENGTH_LONG).show();
+		  service.registerClient(json, new ResultHandler() {
+
+			@Override
+			public void success() {
+				// TODO Auto-generated method stub
+				Toast.makeText(getApplication(), "sucess", Toast.LENGTH_LONG).show();
+			}
+
+			@Override
+			public void fail() {
+				// TODO Auto-generated method stub
+				Toast.makeText(getApplication(), "fail "+ this.getReason(), Toast.LENGTH_LONG).show();
+			}
+
+			@Override
+			public void timeout() {
+				// TODO Auto-generated method stub
+				
+			}
+			  
+		  });
+		  
+		 // Toast.makeText(this, branchName, Toast.LENGTH_LONG).show();
 	  }
 
 }
