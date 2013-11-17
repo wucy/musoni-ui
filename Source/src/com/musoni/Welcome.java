3f052b6cb88cd49b82898ec8a6ec7fb5a5396167
bartekbow <bowtheguitarist@gmail.com>
2013-11-17 02:28:54 +0000

    <--->

--- Source/src/com/musoni/Welcome.java
+++ Source/src/com/musoni/Welcome.java
@@ -11,9 +11,8 @@ public class Welcome extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_welcome);
-		
-		final ActionBar actionBar = getActionBar();
-		BarInflator.inflateActionBar(actionBar);
+	
+		BarInflator.inflateActionBar(this);
 	}
 
 	@Override
