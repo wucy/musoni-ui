fccb73dd0865ab35d74e16cae0d045c3bef907c4
Shijian Li <95588lsj@gmail.com>
2013-11-17 01:33:33 +0000

    Add client register test

---
+++ Source/src/com/musoni/test/ClientRegisterTest.java
@@ -0,0 +1,68 @@
+package com.musoni.test;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.test.AndroidTestCase;
+
+import com.musoni.service.IService;
+import com.musoni.service.ResultHandler;
+import com.musoni.service.ServiceFactory;
+
+public class ClientRegisterTest extends AndroidTestCase {
+	
+	void testClientRegister() throws JSONException {
+		IService service = ServiceFactory.getService();
+		
+		JSONObject json = new JSONObject("{ \"officeId\": 1, \"firstname\": \"Petra\",\"lastname\": \"Yton\", 	\"externalId\": \"786YYH7\", 	\"dateFormat\": \"dd MMMM yyyy\", 	\"locale\": \"en\", 	\"active\": true,	\"activationDate\": \"04 March 2009\"} ");
+	
+		service.authenticate("code4good", "UK2013", new ResultHandler() {
+
+			@Override
+			public void success() {
+				// TODO Auto-generated method stub
+				String r = this.getResult().toString();
+				System.out.println(r);
+			}
+
+			@Override
+			public void fail() {
+				String r = this.getReason();
+				System.out.println(r);
+				
+			}
+
+			@Override
+			public void timeout() {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+		
+		service.registerClient(json, new ResultHandler() {
+
+			@Override
+			public void success() {
+				// TODO Auto-generated method stub
+				String r = this.getResult().toString();
+				System.out.println(r);
+			}
+
+			@Override
+			public void fail() {
+				String r = this.getReason();
+				System.out.println(r);
+				
+			}
+
+			@Override
+			public void timeout() {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+	}
+
+}
