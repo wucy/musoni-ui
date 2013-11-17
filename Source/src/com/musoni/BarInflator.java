993558f9a04dfccea5adfe7efdc0138d86131c23
bartekbow <bowtheguitarist@gmail.com>
2013-11-17 02:28:23 +0000

    Improved bar inflator and Toggle switch keeps state

--- Source/src/com/musoni/BarInflator.java
+++ Source/src/com/musoni/BarInflator.java
@@ -1,16 +1,41 @@
 package com.musoni;
 
 import android.app.ActionBar;
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ToggleButton;
+
+import com.musoni.service.ServiceFactory;
 
 public class BarInflator {
-	public static void inflateActionBar(ActionBar actionBar){
+	
+	private static boolean toggleOn = false;
+	public static void inflateActionBar(Activity activity){
+		final ActionBar actionBar = activity.getActionBar();
 		actionBar.setCustomView(R.layout.actionbar_custom_view_home);
 	     actionBar.setDisplayShowTitleEnabled(true);
 	     actionBar.setDisplayShowCustomEnabled(true);
 	     actionBar.setDisplayUseLogoEnabled(false);
 	     actionBar.setDisplayShowHomeEnabled(false);
-	     actionBar.setOnClick()
+	     
+	     final ToggleButton buttonOne = (ToggleButton) activity.findViewById(R.id.togglebutton);
+	     buttonOne.setChecked(toggleOn);
+	 		buttonOne.setOnClickListener(new Button.OnClickListener() {
+	 	    public void onClick(View v) {
+	 	            if(toggleOn){
+	 	            	buttonOne.setChecked(false);
+	 	            	toggleOn = false;
+	 	            	ServiceFactory.getService().forceOffline();
+	 	            }
+	 	            else{
+	 	            	buttonOne.setChecked(true);
+	 	            	toggleOn = true;
+	 	            	ServiceFactory.getService().forceOnline();
+	 	            }
+	 	    }
+	 	});
 	}
-	
-	public 
 }
+
+
